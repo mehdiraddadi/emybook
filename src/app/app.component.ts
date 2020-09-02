8ebc54fa3fa35dd0b98c02797c06d6624d9356fb
mehdiraddadi <mehdiraddadi@gmail.com>
2020-09-02 16:43:28 +0200

    book project with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'emybook';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyCVXukk_qq3UGvuCd1zfEmDJDAApEnuItw",
+      authDomain: "http-client-42c13.firebaseapp.com",
+      databaseURL: "https://http-client-42c13.firebaseio.com",
+      projectId: "http-client-42c13",
+      storageBucket: "http-client-42c13.appspot.com",
+      messagingSenderId: "585660399583",
+      appId: "1:585660399583:web:3a215ee8865604305b4e6a",
+      measurementId: "G-QYL3BT912P"
+    };
+    firebase.initializeApp(config);
+  }
 }
